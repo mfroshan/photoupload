6d82386e0f831043e920cfcf3e3dda1e9d10aeee
roshan472 <roshanfrancis190@gmail.com>
2024-07-27 17:16:48 +0530

    changes

--- index.js
+++ index.js
@@ -19,6 +19,9 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get('/',(req,res)=>{
+  res.send('Your server is up!')
+})
 // Upload route
 app.post('/api/upload', upload.array('files'), async (req, res) => {
   try {
